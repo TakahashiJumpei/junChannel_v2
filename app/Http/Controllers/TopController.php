1a3a63aa7137a33ea31c04263df8403af70cb5fd
TakahashiJumpei <b1014098@gmail.com>
2022-10-14 23:51:44 +0900

    ベースデザインの実装

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+  public function __invoke()
+  {
+
+    // //DBから最近作成された最新１０件のスレッドを最新順に取得する
+    // $recently_created_threads = Thread::orderBy('id', 'DESC')->take(10)->get();
+    // if ($recently_created_threads->isEmpty()) {
+    //   $recently_created_threads = null;
+    // } else {
+    //   //各スレッドごとのコメント数を取得。スレッドに紐づいているコメントをすべて取得
+    //   foreach ($recently_created_threads as $recently_created_thread) {
+    //     $count_comment = Comment::where('thread_id', $recently_created_thread->id)->get();
+    //     Log::info('$count_comment', [$count_comment]);
+    //     if ($count_comment->isEmpty()) {
+    //       $recently_created_thread->count_comment = 0;
+    //       $recently_created_thread->recently_comment_datetime = null;
+    //     } else {
+    //       $recently_created_thread->count_comment = count($count_comment);
+    //       $recently_comment_datetime = Comment::orderBy('id', 'DESC')->where('thread_id', $recently_created_thread->id)->first();
+    //       Log::info('$recently_comment_datetime', [$recently_comment_datetime]);
+    //       $recently_created_thread->recently_comment_datetime = $recently_comment_datetime->created_at;
+    //     }
+    //     //各スレッドに対してカテゴリ名を紐づける
+    //     $category = Category::where('id', $recently_created_thread->category_id)->first();
+    //     $recently_created_thread->category_name = $category->name;
+    //   }
+    //   Log::info('$recently_created_threads', [$recently_created_threads]);
+    // }
+
+    // //DBからスレッドを最近コメントされた順にソートして取得
+    // //まずコメントテーブルからコメントを外部キーであるスレッドテーブルIDが重複しないように１０件取得する。
+    // //スレッドテーブルが最新のコメント投稿日時順に10種類取得できているので、スレッドをその順番で１０件取得できる。
+    // $recently_comments = Comment::orderBy('id', 'DESC')->whereIn('id', function ($query) {
+    //   $query->select(DB::raw('MAX(id) As id'))->from('comments')->groupBy('thread_id');
+    // })->get();
+    // Log::info('$recently_comments', [$recently_comments]);
+    // if ($recently_comments->isEmpty()) {
+    //   $recently_comments = null;
+    //   $recently_commented_threads = null;
+    // } else {
+    //   $thread_ids = array();
+    //   foreach ($recently_comments as $recently_comment) {
+    //     Log::info('$recently_comment->thread_id', [$recently_comment->thread_id]);
+    //     $thread_ids[] = $recently_comment->thread_id;
+    //   }
+    //   $recently_commented_threads = Thread::whereIn('id', $thread_ids)->orderByRaw('FIELD(id, ' . implode(',', $thread_ids) . ')')->take(10)->get();
+    //   Log::info('$recently_commented_threads', [$recently_commented_threads]);
+
+    //   //各スレッドごとのコメント数を取得。スレッドに紐づいているコメントをすべて取得
+    //   foreach ($recently_commented_threads as $recently_commented_thread) {
+    //     $count_comment = Comment::where('thread_id', $recently_commented_thread->id)->get();
+    //     Log::info('$count_comment', [$count_comment]);
+    //     $recently_commented_thread->count_comment = count($count_comment);
+
+    //     $recently_comment_datetime = Comment::orderBy('id', 'DESC')->where('thread_id', $recently_commented_thread->id)->first();
+    //     Log::info('$recently_comment_datetime', [$recently_comment_datetime]);
+    //     $recently_commented_thread->recently_comment_datetime = $recently_comment_datetime->created_at;
+
+    //     //各スレッドに対してカテゴリ名を紐づける
+    //     $category = Category::where('id', $recently_commented_thread->category_id)->first();
+    //     $recently_commented_thread->category_name = $category->name;
+    //   }
+    //   //dd($recently_commented_threads);
+    // }
+    // Log::info('$recently_commented_threads', [$recently_commented_threads]);
+    // //dd($recently_commented_threads);
+
+    // //////////////////////////////////////////
+    // // DBからすべてのカテゴリを取得
+    // $categories = Category::get();
+    // if ($categories->isEmpty()) {
+    //   $categories = null;
+    // }
+    // Log::info('$categories', [$categories]);
+
+    // return view('top', compact('recently_created_threads', 'recently_commented_threads', 'categories'));
+
+    return view('top');
+  }
+}
---
+++ resources/views/top.blade.php
@@ -0,0 +1,175 @@
+@extends('layout.app')
+
+@section('title', config("app.name"))
+@include('layout.header')
+@include('layout.footer')
+
+
+@section('content')
+
+    {{-- @include('layout.categories_list') --}}
+
+    <div class="col-lg-9">
+
+        {{-- じゅんちゃんねるの簡単な紹介 --}}
+        <div class="h3 mt-5 mb-5">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title font-weight-bold mb-4"><span>じゅんちゃんねるについて</span></h3>
+                    <h5 class="card-text">
+                        じゅんちゃんねるは某有名掲示板を真似た匿名掲示板です。<br>スレッドでのコメントを通じて趣味が合う者同士（雑談でも何でもOK）の楽しい交流の場として利用していただけると管理人としても嬉しいです。<br>基本的にお好きにご利用いただいて構いませんが、最低限のマナーを守って利用しましょう！
+                        </h4>
+                </div>
+            </div>
+        </div>
+
+        {{-- 最近作成されたスレッド一覧の表示 --}}
+        {{-- 最近作成された最新１０件のスレッドを最新順に一覧表示する --}}
+        {{-- <div class="mt-5 mb-5">
+            <table class="table">
+                <thead class="table-bordered table-sm thead-light">
+                    <tr>
+                        <th colspan="1">
+                            最近作成されたスレッド一覧
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="table-bordered table-sm">
+                    @if ($recently_created_threads)
+                        @foreach ($recently_created_threads as $recently_created_thread)
+                            <tr>
+                                <td class="d-flex justify-content-start align-items-center">
+                                    <div>
+                                        <span class="ml-2">[{{ $loop->index + 1 }}]</span>
+                                    </div>
+                                    <div class="d-md-flex justify-content-start align-items-center">
+                                        <div>
+                                            <a href="{{ url('thread/show', $recently_created_thread->id) }}"
+                                                class="btn btn-link">{{ $recently_created_thread->name }}</a>
+                                        </div>
+                                        <div>
+                                            <span class="ml-2">（{{ $recently_created_thread->count_comment }}件）</span>
+                                            @if (isset($recently_created_thread->recently_comment_datetime))
+                                                <span class="ml-2">
+                                                    {{ $recently_created_thread->recently_comment_datetime->format('m月d日 H:i') }}
+                                                </span>
+                                            @else
+                                            @endif
+                                            <span class="ml-3 td-category">
+                                                [ <a
+                                                    href="{{ url('category/show', $recently_created_thread->category_id) }}">{{ $recently_created_thread->category_name }}</a>
+                                                ]
+                                            </span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td>
+                                作成されたスレッドはありません。
+                            </td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div> --}}
+
+        {{-- スレッドランキング（勢いのあるスレッド）の表示。コメントが最新のスレッド順に一覧表示 --}}
+        {{-- <div class="mt-5 mb-5">
+            <table class="table">
+                <thead class="table-bordered table-sm thead-light">
+                    <tr>
+                        <th colspan="1">
+                            勢いのあるスレッド一覧
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="table-bordered table-sm">
+                    @if ($recently_commented_threads)
+                        @foreach ($recently_commented_threads as $recently_commented_thread)
+                            <tr>
+                                <td class="d-flex justify-content-start align-items-center">
+                                    <div>
+                                        <span class="ml-2">[{{ $loop->index + 1 }}]</span>
+                                    </div>
+                                    <div class="d-md-flex justify-content-start align-items-center">
+                                        <div>
+                                            <a href="{{ url('thread/show', $recently_commented_thread->id) }}"
+                                                class="btn btn-link">{{ $recently_commented_thread->name }}</a>
+                                        </div>
+                                        <div>
+                                            <span class="ml-2">（{{ $recently_commented_thread->count_comment }}件）</span>
+                                            <span class="ml-2">
+                                                {{ $recently_commented_thread->recently_comment_datetime->format('m月d日 H:i') }}
+                                            </span>
+                                            <span class="ml-3 td-category">
+                                                [ <a
+                                                    href="{{ url('category/show', $recently_commented_thread->category_id) }}">{{ $recently_commented_thread->category_name }}</a>
+                                                ]
+                                            </span>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            <td>
+                                勢いのあるスレッドはありません。
+                            </td>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div> --}}
+
+        {{-- ※必要に応じて「もっと見る」で各種、別画面でもっと多い件数を表示させる --}}
+
+        {{-- カテゴリ一覧の表示 --}}
+
+    </div>
+
+    <style>
+        @media screen and (max-width:450px) {
+
+            .card-title {
+                font-size: 18px !important;
+            }
+
+            .card-text {
+                font-size: 14px !important;
+            }
+        }
+
+
+        tr {
+            background-color: white;
+        }
+
+        table {
+            table-layout: fixed;
+            width: 100%;
+            word-break: break-all;
+            word-wrap: break-all;
+        }
+
+        @media screen and (max-width:450px) {
+
+            table {
+                font-size: 12px !important;
+            }
+
+            table a {
+                font-size: 14px !important;
+            }
+
+            table .td-category a {
+                font-size: 12px !important;
+            }
+
+        }
+    </style>
+
+@endsection
