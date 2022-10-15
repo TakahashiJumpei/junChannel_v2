7bfcb6e8c88b11360d07e7e8a808c88efc38c5dc
TakahashiJumpei <b1014098@gmail.com>
2022-10-16 01:23:39 +0900

    ベースファイルの修正

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,11 @@
 
 <head>
     <meta charset='utf-8' />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
